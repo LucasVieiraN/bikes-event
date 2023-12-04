9f301591903e1b7e7a445e694ea0bf846d4d48ad
Lucas V <lucasvieirask8@hotmail.com.br>
2023-12-04 12:05:47 -0300

    refactor: removal of signin and signup modules for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { UsersModule } from './modules/users/module/users.module';
 import { TokensModule } from './modules/tokens/module/tokens.module';
-import { SignUpModule } from './modules/sign-up/module/sign-up.module';
-import { SignInModule } from './modules/sign-in/module/sign-in.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, TokensModule, SignUpModule, SignInModule],
+  imports: [UsersModule, TokensModule, AuthModule],
 })
 export class AppModule {}
