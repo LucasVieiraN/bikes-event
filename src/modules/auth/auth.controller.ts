3c897731b1728b247659250c454847d1dd188687
Lucas V <lucasvieirask8@hotmail.com.br>
2023-12-04 12:07:10 -0300

    feat: creation of the auth module

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { Users } from '@prisma/client';
+
+import { AuthService } from './auth.service';
+import { TokensService } from '../tokens/service/tokens.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService, private readonly tokensService: TokensService) {}
+
+  @Post('signin')
+  async login(@Body() payload: any) {
+    return this.authService.signIn(payload.email, payload.password)
+  }
+
+  @Post('register')
+  async register(@Body() user: Users) {
+    return this.authService.signUp(user)
+  }
+}
