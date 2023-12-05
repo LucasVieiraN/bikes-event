b27d415e1a4ec106cbc688d8f4f2908bbc88aad2
Lucas V <lucasvieirask8@hotmail.com.br>
2023-12-05 11:18:39 -0300

    chore: removal 'users' from prisma client

--- src/modules/users/repository/users.repository.ts
+++ src/modules/users/repository/users.repository.ts
@@ -1,16 +1,16 @@
-import { HttpException, Injectable } from "@nestjs/common"
-import { Users } from "@prisma/client"
+import { Injectable } from "@nestjs/common"
 
 import { PrismaService } from "../../../database/prisma.service"
 import { CreateAccountDto } from "../../../modules/auth/dto/create-account.dto"
 import { UpdateUserDto } from "../dto/update-user.dto"
 import { FindUsersOutput } from "../dto/find-users-output"
+import { User } from "../entities/user.entity"
 
 @Injectable()
 export class UsersRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(createAccountDto: CreateAccountDto): Promise<Users> {
+  async create(createAccountDto: CreateAccountDto): Promise<User> {
     return this.prisma.users.create({ data: createAccountDto })
   }
 
@@ -22,15 +22,15 @@ export class UsersRepository {
     return this.prisma.users.findMany({ select: { id: true, name: true, cpf: true, email: true } });
   }
 
-  async findByEmail(email: string): Promise<Users | null> {
+  async findByEmail(email: string): Promise<User | null> {
     return this.prisma.users.findUnique({ where: { email } })
   }
 
-  async findByCPF(cpf: string): Promise<Users | null> {
+  async findByCPF(cpf: string): Promise<User | null> {
     return this.prisma.users.findUnique({ where: { cpf } })
   }
 
-  async updateUser(id: string, updateUserDto: UpdateUserDto): Promise<Users> {
+  async updateUser(id: string, updateUserDto: UpdateUserDto): Promise<User> {
     return this.prisma.users.update({ where: { id }, data: updateUserDto })
   }
 
